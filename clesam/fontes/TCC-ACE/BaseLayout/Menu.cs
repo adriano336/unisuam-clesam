6185d5be7c14a91e2307868460d257bdb5dc812b
adriano336 <adrianochagas336@gmail.com>
2014-05-25 18:46:10 -0300

    Mudança do tipo de conexão com o banco e atribuido métodos.

--- clesam/fontes/TCC-ACE/BaseLayout/Menu.cs
+++ clesam/fontes/TCC-ACE/BaseLayout/Menu.cs
@@ -8,33 +8,35 @@ namespace TCC_ACE.Menu
     public class Menu
     {
 
-        public static string MenuList(Recurso recurso)
+        public static string MenuList(Recurso recurso,int grupo)
         {
 
+
+            var recursos = recurso.Recurso1.Where(s => s.Grupo.Any(g => g.codigo == grupo));
             string html = "";
 
             html += "<li>";
 
-            html += "<a href='#' class='dropdown-toggle'>";
+            html += "<a href='" + recurso.href + "' class='dropdown-toggle'>";
             if (recurso.src != null)
             {
                 html += "<i class='" + recurso.src + "'></i>";
             }
             html += "<span class='menu-text'>"+recurso.titulo+"</span>";
 
-            if (recurso.Recurso1.Count() != 0)
+            if (recursos.Count() != 0)
             {
                 html += "<b class='arrow icon-angle-down'></b>";
             }
             
 
             html += "</a>";
-            if (recurso.Recurso1.Count() != 0)
+            if (recursos.Count() != 0)
             {
                 html += "<ul class='submenu'>";
-                foreach (Recurso r in recurso.Recurso1)
+                foreach (Recurso r in recursos)
                 {
-                    html += MenuList(r);
+                    html += MenuList(r,grupo);
                 }
                 html += "</ul>";
 
@@ -46,17 +48,18 @@ namespace TCC_ACE.Menu
 
         }
 
-        public static string MenuLateral()
+        public static string MenuLateral(int grupo)
         {
 
             ContextEntities context = new ContextEntities();
-            var recursos = context.Recurso.Where(s => s.TipoRecurso.codigo == 1);
+            var recursos = context.Recurso.Where(s => s.TipoRecurso.codigo == 1)
+                                          .Where(s => s.Grupo.Any(g => g.codigo == grupo));
 
             string html = "";
 
             foreach (Recurso r in recursos)
             {
-                html += MenuList(r);
+                html += MenuList(r, grupo);
             }
 
             return html;
---
+++ clesam/fontes/TCC-ACE/BaseLayout/VerificacaoRecurso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace TCC_ACE.BaseLayout
+{
+    public class VerificacaoRecurso
+    {
+        public static Boolean VerificarRecursoPermitido(int grupo, int recurso)
+        {
+            using (var ctx = new ContextEntities())
+            {
+                return ctx.Recurso.Count(r => r.Grupo.Any(g => g.codigo == grupo) && r.codigo == recurso) > 0;
+            }
+        }
+    }
+}
